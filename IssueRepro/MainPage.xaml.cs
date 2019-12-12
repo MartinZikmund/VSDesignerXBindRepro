cc48fd04058e8d038b5240eaafa2cf11b4da9c4f
Martin Zikmund <martinzikmund@live.com>
2019-12-12 17:19:37 +0900

    Design time

--- IssueRepro/MainPage.xaml.cs
+++ IssueRepro/MainPage.xaml.cs
@@ -22,7 +22,7 @@ namespace IssueRepro
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class MainPage : Page, INotifyPropertyChanged
+    public sealed partial class MainPage : Page
     {
         public MainPage()
         {
@@ -30,12 +30,5 @@ namespace IssueRepro
         }
 
         public Visibility RectangleVisibility { get; set; } = Visibility.Collapsed;
-
-        private void RaisePropertyChanged([CallerMemberName]string propertyName = "")
-        {
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-        }
-
-        public event PropertyChangedEventHandler PropertyChanged;
     }
 }
